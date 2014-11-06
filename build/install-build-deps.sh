59027ae88e6c26a1d80b415520506d38131c5cf9
Colin Blundell <blundell@chromium.org>
2014-11-06 16:29:56 +0100

    Fixes in install-build-deps

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -4,29 +4,28 @@ ROOT_DIR="$(dirname $(realpath $(dirname "${BASH_SOURCE[0]}")))"
 BUILD_DIR="$ROOT_DIR/build"
 BUILDTOOLS_DIR="$ROOT_DIR/buildtools"
 THIRD_PARTY_DIR="$ROOT_DIR/third_party/"
+mkdir -p $THIRD_PARTY_DIR
 
 # BODY
 
 # Install the Mojo SDK and shell.
 
-# TODO(blundell): Fetch the Mojo SDK via git clone.
 cd $THIRD_PARTY_DIR
 rm -rf mojo
 git clone https://github.com/colinblundell/mojo-sdk.git mojo
 $THIRD_PARTY_DIR/mojo/build/install-build-deps.sh
 
-# Install the Mojo shell
-# TODO(blundell): Should this script be provided by the SDK?
-cd $ROOT_DIR
-$BUILD_DIR/download_mojo_shell.py
-
-# Install things for building with GN and Ninja.
-
-# Install gsutil.
+# Install gsutil (required by download_mojo_shell.py).
 cd $THIRD_PARTY_DIR
 curl --remote-name https://storage.googleapis.com/pub/gsutil.tar.gz
 tar xfz gsutil.tar.gz
 rm gsutil.tar.gz
+
+# TODO(blundell): Should this script be provided by the SDK?
+cd $ROOT_DIR
+$BUILD_DIR/download_mojo_shell.py
+
+# Install and set up the environment for a GN + Ninja build.
 
 # Install gn.
 $THIRD_PARTY_DIR/gsutil/gsutil cp gs://chromium-gn/56e78e1927e12e5c122631b7f5a46768e527f1d2 $BUILDTOOLS_DIR/gn
