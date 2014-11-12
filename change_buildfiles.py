0d69f9d24df666e82cc6dfd28a775541323b16fd
Colin Blundell <blundell@chromium.org>
2014-11-12 13:14:45 +0100

    change_buildfiles.py

--- change_buildfiles.py
+++ change_buildfiles.py
@@ -1,5 +1,6 @@
 [
 # Changing the dependencies on //mojo to be relative.
-[r'"//(mojo[^"]*")', r'rebase_path("\1, ".", mojo_root)', ['examples*BUILD.gn', "*mojom.gni"]],
+[r'"//(mojo[^"]*")', r'rebase_path("\1, ".", mojo_root)', ['examples*BUILD.gn']],
 # Adding the import of //build/config/mojo.gni.
-[r'(LICENSE file.)', r'\1\nimport("//build/config/mojo.gni")', ['examples*BUILD.gn', "*mojom.gni"]]
+[r'(LICENSE file.)', r'\1\nimport("//build/config/mojo.gni")', ['examples*BUILD.gn']]
+]
