29b632ac3ccc87ed3f36bd389bace174c7802f7d
Colin Blundell <blundell@chromium.org>
2014-11-12 14:24:10 +0100

    Brought in tools to make the SDK consumable

--- tools/rev_sdk.sh
+++ tools/rev_sdk.sh
@@ -8,6 +8,7 @@ VERSION=`cat $VERSION_FILE`
 
 cd $MOJO_SRC_DIR
 git checkout $VERSION
+./tools/consumable_sdk/make_sdk_consumable.sh $MOJO_SRC_DIR $CHROMIUM_SRC_DIR
 
 cd $ROOT_DIR
 ./tools/rev_sdk.py $MOJO_SRC_DIR $CHROMIUM_SRC_DIR
