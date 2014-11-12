9a38bcfe097cfdeb350d52389c0a26af57900eda
Colin Blundell <blundell@chromium.org>
2014-11-12 13:07:17 +0100

    Update rev_sdk.py

--- tools/rev_sdk.py
+++ tools/rev_sdk.py
@@ -7,34 +7,25 @@ import os
 import subprocess
 import sys
 
-current_path = os.path.dirname(os.path.realpath(__file__))
-root_path = os.path.join(current_path, "..")
-
-# Evaluate mojo.gni to obtain mojo_root and mojo_third_party_root.
-mojo_gni_file = os.path.join(root_path, "build/config/mojo.gni")
-execfile(mojo_gni_file)
-assert(mojo_root.startswith("//"))
-mojo_sdk_dir = os.path.join(mojo_root[2:], "mojo")
-assert(mojo_third_party_root.startswith("//"))
-mojo_third_party_dir = os.path.join(mojo_third_party_root[2:], "third_party")
-
-dirs_to_clone = {
+sdk_dirs_to_clone = [
   # The core SDK.
-  "mojo/public" : os.path.join(mojo_sdk_dir, "/public"),
+  "mojo/public",
 
   # The build dependencies of the core SDK.
-  "third_party/cython" : os.path.join(mojo_third_party_dir, "cython"),
-  "third_party/khronos" : os.path.join(mojo_third_party_dir, "khronos"),
-  "build/secondary/testing/gtest" : os.path.join(mojo_sdk_dir, "build/secondary/testing/gtest"),
+  "third_party/cython",
+  "third_party/khronos",
+  "build/secondary/testing/gtest",
+]
 
+client_dirs_to_clone = [
   # Client apps.
   # TODO(blundell): Copy in stuff for build/.
-  "examples/apptest" : "apptest",
-  "examples/echo" : "echo",
+  "examples/apptest",
+  "examples/echo",
 
   # Support for a gn/ninja client build.
-  "tools/generate_library_loader" : "tools/generate_library_loader",
-}
+  "tools/generate_library_loader",
+]
 
 def system(command):
   return subprocess.check_output(command)
@@ -42,11 +33,16 @@ def system(command):
 def commit(message):
   subprocess.call(['git', 'commit', '-a', '-m', message])
 
-def rev(source_dir, target_dir):
+def rev(source_dir, target_dir, dirs_to_clone):
   os.chdir(source_dir)
   src_commit = system(["git", "show-ref", "HEAD", "-s"]).strip()
 
-  for input_dir, output_dir in dirs_to_clone.iteritems():
+  for input_dir in dirs_to_clone:
+    output_dir = input_dir 
+
+    # Strip any "mojo/" prefixes to avoid stutter.
+    if output_dir.startswith("mojo/"):
+      output_dir = output_dir[len("mojo/":)]
     os.chdir(target_dir)
     if os.path.exists(output_dir):
       print "removing directory %s" % output_dir
@@ -72,4 +68,15 @@ if len(sys.argv) != 2:
   print "usage: rev_sdk.py <mojo source dir>"
   sys.exit(1)
 
-rev(sys.argv[1], root_path)
+# Evaluate mojo.gni to obtain mojo_root and mojo_third_party_root.
+current_path = os.path.dirname(os.path.realpath(__file__))
+root_path = os.path.join(current_path, "..")
+mojo_gni_file = os.path.join(root_path, "build/config/mojo.gni")
+execfile(mojo_gni_file)
+assert(mojo_root.startswith("//"))
+mojo_sdk_dir = os.path.join(mojo_root[2:], "mojo")
+assert(mojo_third_party_root.startswith("//"))
+mojo_third_party_dir = os.path.join(mojo_third_party_root[2:], "third_party")
+
+rev(sys.argv[1], mojo_sdk_dir, sdk_dirs_to_clone)
+rev(sys.argv[1], root_path, client_dirs_to_clone)
