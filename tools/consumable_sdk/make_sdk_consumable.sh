bc28ba02a04d2b29fbfb39d01fbfdbdc1287aa29
Colin Blundell <blundell@chromium.org>
2014-11-13 16:41:22 +0100

    Update consumable SDK

--- tools/consumable_sdk/make_sdk_consumable.sh
+++ tools/consumable_sdk/make_sdk_consumable.sh
@@ -7,6 +7,7 @@ cd $MOJO_SRC_DIR
 git apply $SCRIPT_DIR/new_buildfiles.patch
 $CHROMIUM_SRC_DIR/tools/git/mffr.py -fi $SCRIPT_DIR/change_gn.py
 git apply $SCRIPT_DIR/bindings_generation.patch
-git apply $SCRIPT_DIR/python_buildfile.patch
+# TODO(blundell): Talk to brettw about why this patch breaks gn gen.
+#git apply $SCRIPT_DIR/python_buildfile.patch
 git add mojo/build
 git add build/config/mojo.gni
