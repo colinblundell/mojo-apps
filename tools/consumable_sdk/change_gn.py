6b9db0657bfe74e9cecde261706e765da6a23cfb
Colin Blundell <blundell@chromium.org>
2014-11-13 15:50:17 +0100

    Changes to consumable SDK tools

--- tools/consumable_sdk/change_gn.py
+++ tools/consumable_sdk/change_gn.py
@@ -1,12 +1,12 @@
 [
 # Changing the dependencies on //mojo to be relative.
-[r'"//(mojo[^"]*")', r'rebase_path("\1, ".", mojo_root)', ['mojo/public*BUILD.gn', "*mojom.gni"]],
+[r'"//(mojo/public[^"]*")', r'rebase_path("\1, ".", mojo_root)', ['mojo/public*BUILD.gn', 'mojo/edk*BUILD.gn', 'mojo/services/public*BUILD.gn', "*mojom.gni"]],
 # Changing the dependencies on //third_party to be relative.
 [r'"//(third_party[^"]*")', r'rebase_path("\1, ".", mojo_third_party_root)', ['mojo/public*BUILD.gn', "*mojom.gni"]],
 # Changing the dependencies on //testing to be relative.
 [r'"//(testing[^"]*")', r'rebase_path("\1, ".", mojo_third_party_root)', ['mojo/public*BUILD.gn', "*mojom.gni"]],
 # Adding the import of //build/config/mojo.gni.
-[r'(LICENSE file.)', r'\1\nimport("//build/config/mojo.gni")', ['mojo/public*BUILD.gn', "*mojom.gni"]],
+[r'(LICENSE file.)', r'\1\n\nimport("//build/config/mojo.gni")', ['mojo/public*BUILD.gn', 'mojo/edk*BUILD.gn', 'mojo/services/public*BUILD.gn', "*mojom.gni"]],
 # Adding the public config.
 [r'([ ]+)(sources =.*)', r'\1public_configs = [ rebase_path("mojo/build/config", ".", mojo_root) + ":mojo_sdk" ]\n\1\2', ['mojo/public*BUILD.gn']],
 # Removing the public config from BUILD.gn files that don't use it to avoid
