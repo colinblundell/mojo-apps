43cb9a309e4d6154ef6672a56acfc2c298efee1e
Colin Blundell <blundell@chromium.org>
2014-11-18 13:15:24 +0100

    Update to consumable SDK

--- client_tools/consumable_sdk/change_gn.py
+++ client_tools/consumable_sdk/change_gn.py
@@ -6,7 +6,7 @@
 # Changing the dependencies on //testing to be relative.
 [r'"//(testing[^"]*")', r'rebase_path("\1, ".", mojo_third_party_root)', ['mojo/public*BUILD.gn', "*mojo/public*.gni"]],
 # Adding the import of //build/config/mojo.gni.
-[r'(LICENSE file.)', r'\1\n\nimport("//build/config/mojo.gni")', ['mojo/public*BUILD.gn', 'mojo/edk*BUILD.gn', 'mojo/services/public*BUILD.gn', "*mojom.gni"]],
+[r'(LICENSE file.)', r'\1\n\nimport("//build/config/mojo.gni")', ['mojo/public*BUILD.gn', 'mojo/edk*BUILD.gn', 'mojo/services/public*BUILD.gn', "*mojom.gni", "*mojo_application.gni"]],
 # Adding the public config.
 [r'([ ]+)(sources =.*)', r'\1public_configs = [ rebase_path("mojo/build/config", ".", mojo_root) + ":mojo_sdk" ]\n\1\2', ['mojo/public*BUILD.gn']],
 # Removing the public config from BUILD.gn files that don't use it to avoid
