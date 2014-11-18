04aa6bc772d6d3567a8e91d784f162468a40eab8
Colin Blundell <blundell@chromium.org>
2014-11-18 13:00:54 +0100

    Update consumable SDK/services processes

--- client_tools/consumable_public_services/make_services_consumable.sh
+++ client_tools/consumable_public_services/make_services_consumable.sh
@@ -8,11 +8,11 @@ git apply $SCRIPT_DIR/new_buildfiles.patch
 $CHROMIUM_SRC_DIR/tools/git/mffr.py -fi $SCRIPT_DIR/change_gn.py
 git apply $SCRIPT_DIR/bindings_generation.patch
 git apply $SCRIPT_DIR/mojo_gni.patch
-git apply $SCRIPT_DIR/add_missing_dependencies.patch
 git apply $SCRIPT_DIR/presubmit_fixup.patch
 git apply $SCRIPT_DIR/mojom_public_configs.patch
 git apply $SCRIPT_DIR/mojom_import_dirs.patch
 git apply $SCRIPT_DIR/fixup_mojom_gni_comments.patch
 git apply $SCRIPT_DIR/mojo_gni_import_dirs.patch
+git apply $SCRIPT_DIR/add_missing_dependencies.patch
 git add mojo/services/build
 git add mojo/services/public/DEPS
--- client_tools/consumable_sdk/make_sdk_consumable.sh
+++ client_tools/consumable_sdk/make_sdk_consumable.sh
@@ -7,7 +7,5 @@ cd $MOJO_SRC_DIR
 git apply $SCRIPT_DIR/new_buildfiles.patch
 $CHROMIUM_SRC_DIR/tools/git/mffr.py -fi $SCRIPT_DIR/change_gn.py
 git apply $SCRIPT_DIR/bindings_generation.patch
-git apply $SCRIPT_DIR/rebase_list_example.patch
-git apply $SCRIPT_DIR/add_missing_deps.patch
 git add mojo/build
 git add build/config/mojo.gni
