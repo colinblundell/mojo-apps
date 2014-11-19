b5371e4a799d5b1be4e6304577daa865d5d77e39
Colin Blundell <blundell@chromium.org>
2014-11-19 11:45:32 +0100

    Chromium pull changes

--- client_tools/chromium_pull_changes/make_chromium_pull_changes.sh
+++ client_tools/chromium_pull_changes/make_chromium_pull_changes.sh
@@ -4,7 +4,9 @@ CHROMIUM_SRC_DIR=$1
 
 cd $CHROMIUM_SRC_DIR
 git apply $SCRIPT_DIR/mojo_gni.patch
-$CHROMIUM_SRC_DIR/tools/git/mffr.py -fi $SCRIPT_DIR/change_gn.py
+$CHROMIUM_SRC_DIR/tools/git/mffr.py -fi $SCRIPT_DIR/search_and_replace_changes.py
 git apply $SCRIPT_DIR/add_landmine.patch
 git apply $SCRIPT_DIR/get_mojo_to_build.patch
+git apply $SCRIPT_DIR/get_content_to_build.patch
+git apply $SCRIPT_DIR/get_chrome_to_build.patch
 git add build/config/mojo.gni
--- client_tools/chromium_pull_changes/search_and_replace_changes.py
+++ client_tools/chromium_pull_changes/search_and_replace_changes.py
@@ -1,10 +1,13 @@
 [
 # Changing the dependencies on //mojo/public.
 [r'("//)(mojo/public[^"]*")', r'\1third_party/mojo/\2', ['*BUILD.gn']],
+[r'(\.\./)(mojo/public)', r'\1third_party/mojo/\2', ['*']],
 # Changing the dependencies on //mojo/services/public.
 [r'("//)(mojo/services/public[^"]*")', r'\1third_party/\2', ['*BUILD.gn']],
+[r'(\.\./)(mojo/services/public)', r'\1third_party/\2', ['*']],
 # Changing the dependencies on //mojo/edk.
 [r'("//)(mojo/edk[^"]*")', r'\1third_party/mojo/\2', ['*BUILD.gn']],
+[r'(\.\./)(mojo/edk)', r'\1third_party/mojo/\2', ['*']],
 # Changing the include paths to be relative.
 [r'"mojo/services/public', r'"services/public', ['*cc', '*h', '*mojom']],
 ]
