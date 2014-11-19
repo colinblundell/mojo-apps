52f232177e974121fcfe94219160cdd338f8a832
Colin Blundell <blundell@chromium.org>
2014-11-19 13:51:09 +0100

    Update mojo sdk to rev 504bae721b981f781c5a0b9c8c150e7f246fb88b

--- client_tools/install-build-deps.sh
+++ client_tools/install-build-deps.sh
@@ -8,57 +8,6 @@ mkdir -p $BUILDTOOLS_DIR
 THIRD_PARTY_DIR="$ROOT_DIR/third_party/"
 mkdir -p $THIRD_PARTY_DIR
 
-GTEST_DIR="$ROOT_DIR/testing/gtest"
-
-function install_dep_from_tarfile {
-  SRC_URL=$1
-  PACKAGE_DIR=$2
-  FILENAME="$(basename $SRC_URL)"
-
-  DOWNLOAD_DIR="$(basename $FILENAME .tar.gz)/$PACKAGE_DIR"
-  INSTALL_DIR="$THIRD_PARTY_DIR/$PACKAGE_DIR"
-  OLD_DIR="$THIRD_PARTY_DIR/$PACKAGE_DIR.old"
-
-  mkdir -p "$INSTALL_DIR"
-  cd "$INSTALL_DIR"
-  echo "Downloading $SRC_URL"
-  curl --remote-name "$SRC_URL"
-  tar xvzf "$FILENAME"
-
-  cd "$DOWNLOAD_DIR"
-
-  # Replace with new directory
-  cd "$ROOT_DIR"
-  mv "$INSTALL_DIR" "$OLD_DIR"
-  mv "$OLD_DIR/$DOWNLOAD_DIR" "$INSTALL_DIR"
-  rm -fr "$OLD_DIR"
-}
-
-# BODY
-
-# Download and extract PLY.
-# Homepage:
-# http://dabeaz.com/ply
-install_dep_from_tarfile "http://dabeaz.com/ply/ply-3.4.tar.gz" "ply"
-
-# Download and extract Jinja2.
-# Homepage:
-# http://jinja.pocoo.org/
-# Installation instructions:
-# http://jinja.pocoo.org/docs/intro/#from-the-tarball-release
-# Download page:
-# https://pypi.python.org/pypi/Jinja2
-JINJA2_SRC_URL="https://pypi.python.org/packages/source/"
-JINJA2_SRC_URL+="J/Jinja2/Jinja2-2.7.1.tar.gz"
-install_dep_from_tarfile $JINJA2_SRC_URL 'jinja2'
-
-# Download and extract MarkupSafe.
-# Homepage:
-# https://pypi.python.org/pypi/MarkupSafe
-MARKUPSAFE_SRC_URL="https://pypi.python.org/packages/source/"
-MARKUPSAFE_SRC_URL+="M/MarkupSafe/MarkupSafe-0.23.tar.gz"
-install_dep_from_tarfile $MARKUPSAFE_SRC_URL 'markupsafe'
-
 # Extract the Mojo SDK root from //build/config/mojo.gni.
 MOJO_SDK_ROOT=`\grep "mojo_root = " build/config/mojo.gni | cut -d"=" -f2 | tr -d '" '`
 # Strip the "//" from the beginning.
