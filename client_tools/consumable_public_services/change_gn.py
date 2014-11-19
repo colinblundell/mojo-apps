4ba10cd0c08f2a0bad7c0c21035fe16bf6e152a8
Colin Blundell <blundell@chromium.org>
2014-11-19 19:38:02 +0100

    Change consumable_public_services scripts

--- client_tools/consumable_public_services/change_gn.py
+++ client_tools/consumable_public_services/change_gn.py
@@ -1,15 +1,17 @@
 [
 # Changing the dependencies on //mojo/services/public to be relative.
-[r'"//mojo/(services/public[^"]*")', r'rebase_path("\1, ".", mojo_services_root)', ['mojo/public*BUILD.gn', 'mojo/edk*BUILD.gn', 'mojo/services/public*BUILD.gn', "*mojom.gni"]],
+#[r'"//mojo/(services/public[^"]*")', r'rebase_path("\1, ".", mojo_services_root)', ['mojo/public*BUILD.gn', 'mojo/edk*BUILD.gn', 'mojo/services/public*BUILD.gn', "*mojom.gni"]],
 # Changing the dependencies on //third_party to be relative.
-[r'"//(third_party[^"]*")', r'rebase_path("\1, ".", mojo_third_party_root)', ['mojo/services/public*BUILD.gn']],
+#[r'"//(third_party[^"]*")', r'rebase_path("\1, ".", mojo_third_party_root)', ['mojo/services/public*BUILD.gn']],
 # Changing the dependencies on //testing to be relative.
-[r'"//(testing[^"]*")', r'rebase_path("\1, ".", mojo_third_party_root)', ['mojo/services/public*BUILD.gn']],
+#[r'"//(testing[^"]*")', r'rebase_path("\1, ".", mojo_third_party_root)', ['mojo/services/public*BUILD.gn']],
 # Adding the public configs.
 #[r'([ ]+)(sources =.*)', r'\1public_configs = [ rebase_path("mojo/build/config", ".", mojo_root) + ":mojo_sdk" ]\n\1\2', ['mojo/services/public/*BUILD.gn']],
-[r'([ ]+)(sources =.*)', r'\1public_configs = [\n\1  rebase_path("services/build/config", ".", mojo_services_root) + ":services"\n\1]\n\n\1\2', ['mojo/services/public/*BUILD.gn']],
+[r'(LICENSE file.)', r'\1\n\nimport("//build/config/mojo.gni")', ['mojo/services/public*BUILD.gn']],
+[r'"//(mojo/public[^"]*")', r'rebase_path("\1, ".", mojo_root)', ['mojo/services/public*BUILD.gn']],
+#[r'([ ]+)(sources =.*)', r'\1public_configs = [ "../../../build/config:services" ]\n\n\1\2', ['mojo/services/public/*BUILD.gn']],
 # Changing the include paths to be relative.
-[r'"mojo/services/public', r'"services/public', ['mojo/services/public*cc', 'mojo/services/public*h', 'mojo/services/public*mojom']],
+#[r'"mojo/services/public', r'"services/public', ['mojo/services/public*cc', 'mojo/services/public*h', 'mojo/services/public*mojom']],
 # Removing the public config from BUILD.gn files that don't use it to avoid
 # gn errors.
 #[r'[ ]*public_configs = \[ rebase_path\("mojo/build/config", ".", mojo_root\) \+ ":mojo_sdk" \]', '', ["*mojo/services/public/interfaces/content_handler/BUILD.gn", "*mojo/services/public/interfaces/network/BUILD.gn"]],
--- client_tools/consumable_public_services/make_services_consumable.sh
+++ client_tools/consumable_public_services/make_services_consumable.sh
@@ -4,15 +4,15 @@ MOJO_SRC_DIR=$1
 CHROMIUM_SRC_DIR=$2
 
 cd $MOJO_SRC_DIR
-git apply $SCRIPT_DIR/new_buildfiles.patch
+#git apply $SCRIPT_DIR/new_buildfiles.patch
 $CHROMIUM_SRC_DIR/tools/git/mffr.py -fi $SCRIPT_DIR/change_gn.py
-git apply $SCRIPT_DIR/bindings_generation.patch
-git apply $SCRIPT_DIR/mojo_gni.patch
-git apply $SCRIPT_DIR/presubmit_fixup.patch
-git apply $SCRIPT_DIR/mojom_public_configs.patch
-git apply $SCRIPT_DIR/mojom_import_dirs.patch
-git apply $SCRIPT_DIR/fixup_mojom_gni_comments.patch
-git apply $SCRIPT_DIR/mojo_gni_import_dirs.patch
-git apply $SCRIPT_DIR/add_missing_dependencies.patch
-git add mojo/services/build
-git add mojo/services/public/DEPS
+#git apply $SCRIPT_DIR/bindings_generation.patch
+#git apply $SCRIPT_DIR/mojo_gni.patch
+#git apply $SCRIPT_DIR/presubmit_fixup.patch
+#git apply $SCRIPT_DIR/mojom_public_configs.patch
+#git apply $SCRIPT_DIR/mojom_import_dirs.patch
+#git apply $SCRIPT_DIR/fixup_mojom_gni_comments.patch
+#git apply $SCRIPT_DIR/mojo_gni_import_dirs.patch
+#git apply $SCRIPT_DIR/add_missing_dependencies.patch
+#git add mojo/services/build
+#git add mojo/services/public/DEPS
