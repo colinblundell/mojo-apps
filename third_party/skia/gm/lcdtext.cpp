d5bb9629cf101d9beb0e57900206d086d40fe1a6
Colin Blundell <blundell@chromium.org>
2014-11-19 14:02:11 +0100

    Update dirs copied in from the Mojo repo

---
+++ third_party/skia/gm/lcdtext.cpp
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+
+/* Tests text rendering with LCD and subpixel rendering turned on and off.
+ */
+
+#include "gm.h"
+#include "SkCanvas.h"
+
+class LcdTextGM : public skiagm::GM {
+public:
+    LcdTextGM() {
+        const int pointSize = 36;
+        textHeight = SkIntToScalar(pointSize);
+    }
+    
+protected:
+    
+    SkString onShortName() {
+        return SkString("lcdtext");
+    }
+    
+    SkISize onISize() { return SkISize::Make(640, 480); }
+    
+    virtual void onDraw(SkCanvas* canvas) {
+        
+        y = textHeight;
+        drawText(canvas, SkString("TEXT: SubpixelTrue LCDRenderTrue"),
+                 true,  true);
+        drawText(canvas, SkString("TEXT: SubpixelTrue LCDRenderFalse"),
+                 true,  false);
+        drawText(canvas, SkString("TEXT: SubpixelFalse LCDRenderTrue"),
+                 false, true);
+        drawText(canvas, SkString("TEXT: SubpixelFalse LCDRenderFalse"),
+                 false, false);
+    }
+    
+    void drawText(SkCanvas* canvas, const SkString& string,
+                  bool subpixelTextEnabled, bool lcdRenderTextEnabled) {
+        SkPaint paint;
+        paint.setColor(SK_ColorBLACK);
+        paint.setDither(true);
+        paint.setAntiAlias(true);
+        sk_tool_utils::set_portable_typeface(&paint);
+        paint.setSubpixelText(subpixelTextEnabled);
+        paint.setLCDRenderText(lcdRenderTextEnabled);
+        paint.setTextSize(textHeight);
+        
+        canvas->drawText(string.c_str(), string.size(), 0, y, paint);
+        y += textHeight;
+    }
+    
+private:
+    typedef skiagm::GM INHERITED;
+    SkScalar y, textHeight;
+};
+
+/*
+ *  Skia will automatically disable LCD requests if the total size exceeds some limit
+ *  (hard coded in this test for now, as it is now avaiable as an API)
+ *
+ *  Test this both by changing "textsize" and by changing the computed size (textsize * CTM)
+ */
+class LcdTextSizeGM : public skiagm::GM {
+    enum {
+        kLCDTextSizeLimit = 48
+    };
+
+    static void ScaleAbout(SkCanvas* canvas, SkScalar sx, SkScalar sy, SkScalar px, SkScalar py) {
+        SkMatrix m;
+        m.setScale(sx, sy, px, py);
+        canvas->concat(m);
+    }
+
+public:
+    LcdTextSizeGM() {}
+    
+protected:
+    SkString onShortName() {
+        return SkString("lcdtextsize");
+    }
+    
+    SkISize onISize() { return SkISize::Make(320, 120); }
+    
+    virtual void onDraw(SkCanvas* canvas) {
+        const char* lcd_text = "LCD";
+        const char* gray_text = "GRAY";
+
+        SkPaint paint;
+        paint.setAntiAlias(true);
+        paint.setLCDRenderText(true);
+
+        const struct {
+            SkPoint     fLoc;
+            SkScalar    fTextSize;
+            SkScalar    fScale;
+            const char* fText;
+        } rec[] = {
+            { {  10,  50 }, kLCDTextSizeLimit - 1,     1,  lcd_text },
+            { { 160,  50 }, kLCDTextSizeLimit + 1,     1,  gray_text },
+            { {  10, 100 }, kLCDTextSizeLimit / 2, 1.99f,  lcd_text },
+            { { 160, 100 }, kLCDTextSizeLimit / 2, 2.01f,  gray_text },
+        };
+
+        for (size_t i = 0; i < SK_ARRAY_COUNT(rec); ++i) {
+            const SkPoint loc = rec[i].fLoc;
+            SkAutoCanvasRestore acr(canvas, true);
+
+            paint.setTextSize(rec[i].fTextSize);
+            ScaleAbout(canvas, rec[i].fScale, rec[i].fScale, loc.x(), loc.y());
+            canvas->drawText(rec[i].fText, strlen(rec[i].fText), loc.x(), loc.y(), paint);
+        }
+    }
+
+private:
+    typedef skiagm::GM INHERITED;
+};
+
+#include "SkSurface.h"
+
+// ensure that we respect the SkPixelGeometry in SurfaceProps
+class LcdTextProps : public skiagm::GM {
+    static void DrawText(SkCanvas* canvas) {
+        canvas->drawColor(SK_ColorWHITE);
+        SkPaint paint;
+        paint.setAntiAlias(true);
+        paint.setLCDRenderText(true);
+        paint.setTextSize(30);
+        canvas->drawText("Base", 4, 4, 30, paint);
+        canvas->saveLayer(NULL, NULL);
+        canvas->drawText("Layer", 5, 4, 70, paint);
+        canvas->restore();
+    }
+
+protected:
+    SkString onShortName() SK_OVERRIDE {
+        return SkString("lcdtextprops");
+    }
+
+    SkISize onISize() SK_OVERRIDE { return SkISize::Make(230, 120); }
+
+    uint32_t onGetFlags() const SK_OVERRIDE {
+        return kSkip565_Flag;
+    }
+
+    virtual void onDraw(SkCanvas* canvas) SK_OVERRIDE {
+        const SkPixelGeometry geos[] = {
+            kRGB_H_SkPixelGeometry,
+            kUnknown_SkPixelGeometry,
+        };
+
+        const SkImageInfo info = SkImageInfo::MakeN32Premul(100, 100);
+        for (size_t i = 0; i < SK_ARRAY_COUNT(geos); ++i) {
+            SkSurfaceProps props = SkSurfaceProps(0, geos[i]);
+            SkAutoTUnref<SkSurface> surf(canvas->newSurface(info, &props));
+            if (!surf) {
+                surf.reset(SkSurface::NewRaster(info, &props));
+            }
+            DrawText(surf->getCanvas());
+            surf->draw(canvas, SkIntToScalar(i * (info.width() + 10)), 0, NULL);
+        }
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+DEF_GM( return new LcdTextGM; )
+DEF_GM( return new LcdTextSizeGM; )
+// Temporarily disabled (dftext interference)
+// DEF_GM( return new LcdTextProps; )
---
+++ third_party/skia/src/gpu/GrGpuResource.cpp
@@ -0,0 +1,122 @@
+
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+
+#include "GrGpuResource.h"
+#include "GrResourceCache2.h"
+#include "GrGpu.h"
+
+static inline GrResourceCache2* get_resource_cache2(GrGpu* gpu) {
+    SkASSERT(gpu);
+    SkASSERT(gpu->getContext());
+    SkASSERT(gpu->getContext()->getResourceCache2());
+    return gpu->getContext()->getResourceCache2();
+}
+
+static inline GrResourceCache* get_resource_cache(GrGpu* gpu) {
+    SkASSERT(gpu);
+    SkASSERT(gpu->getContext());
+    SkASSERT(gpu->getContext()->getResourceCache());
+    return gpu->getContext()->getResourceCache();
+}
+
+GrGpuResource::GrGpuResource(GrGpu* gpu, bool isWrapped)
+    : fGpu(gpu)
+    , fCacheEntry(NULL)
+    , fGpuMemorySize(kInvalidGpuMemorySize)
+    , fUniqueID(CreateUniqueID())
+    , fScratchKey(GrResourceKey::NullScratchKey())
+    , fContentKeySet(false) {
+    if (isWrapped) {
+        fFlags = kWrapped_FlagBit;
+    } else {
+        fFlags = 0;
+    }
+}
+
+void GrGpuResource::registerWithCache() {
+    get_resource_cache2(fGpu)->insertResource(this);
+}
+
+GrGpuResource::~GrGpuResource() {
+    // subclass should have released this.
+    SkASSERT(this->wasDestroyed());
+}
+
+void GrGpuResource::release() { 
+    if (fGpu) {
+        this->onRelease();
+        get_resource_cache2(fGpu)->removeResource(this);
+        fGpu = NULL;
+    }
+}
+
+void GrGpuResource::abandon() {
+    if (fGpu) {
+        this->onAbandon();
+        get_resource_cache2(fGpu)->removeResource(this);
+        fGpu = NULL;
+    }
+}
+
+const GrContext* GrGpuResource::getContext() const {
+    if (fGpu) {
+        return fGpu->getContext();
+    } else {
+        return NULL;
+    }
+}
+
+GrContext* GrGpuResource::getContext() {
+    if (fGpu) {
+        return fGpu->getContext();
+    } else {
+        return NULL;
+    }
+}
+
+bool GrGpuResource::setContentKey(const GrResourceKey& contentKey) {
+    // Currently this can only be called once and can't be called when the resource is scratch.
+    SkASSERT(!contentKey.isScratch());
+    SkASSERT(this->internalHasRef());
+    
+    if (fContentKeySet) {
+        return false;
+    }
+
+    fContentKey = contentKey;
+    fContentKeySet = true;
+
+    if (!get_resource_cache2(fGpu)->didSetContentKey(this)) {
+        fContentKeySet = false;
+        return false;
+    }
+    return true;
+}
+
+void GrGpuResource::notifyIsPurgable() const {
+    if (fCacheEntry && !this->wasDestroyed()) {
+        get_resource_cache(fGpu)->notifyPurgable(this);
+    }
+}
+
+void GrGpuResource::setScratchKey(const GrResourceKey& scratchKey) {
+    SkASSERT(fScratchKey.isNullScratch());
+    SkASSERT(scratchKey.isScratch());
+    SkASSERT(!scratchKey.isNullScratch());
+    fScratchKey = scratchKey;
+}
+
+uint32_t GrGpuResource::CreateUniqueID() {
+    static int32_t gUniqueID = SK_InvalidUniqueID;
+    uint32_t id;
+    do {
+        id = static_cast<uint32_t>(sk_atomic_inc(&gUniqueID) + 1);
+    } while (id == SK_InvalidUniqueID);
+    return id;
+}
