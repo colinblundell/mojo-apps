c179637fd4d8e148b481a70f0c3aeaf14d20854b
Colin Blundell <blundell@chromium.org>
2014-12-02 13:48:45 +0100

    Chromium pull changes

--- client_tools/chromium_pull_changes/make_chromium_pull_changes.sh
+++ client_tools/chromium_pull_changes/make_chromium_pull_changes.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 SCRIPT_DIR="$(realpath $(dirname "${BASH_SOURCE[0]}"))"
-CHROMIUM_SRC_DIR=$1
+MOJO_SRC_DIR=$1
+CHROMIUM_SRC_DIR=$2
 
 cd $CHROMIUM_SRC_DIR
+echo "Moving code from Mojo public repo to live under //third_party/mojo"
+rm -rf third_party/mojo
+mkdir -p third_party/mojo/mojo
+mkdir -p third_party/mojo/services
+git mv mojo/public third_party/mojo/mojo/public
+git mv mojo/edk third_party/mojo/mojo/edk
+git mv mojo/services/public third_party/mojo/services/public
+echo "Applying mojo_gni.patch"
 git apply $SCRIPT_DIR/mojo_gni.patch
 $CHROMIUM_SRC_DIR/tools/git/mffr.py -fi $SCRIPT_DIR/search_and_replace_changes.py
+echo "Applying add_landmine.patch"
 git apply $SCRIPT_DIR/add_landmine.patch
+echo "Applying gypfile_references_to_logging_files.patch"
+git apply $SCRIPT_DIR/gypfile_references_to_logging_files.patch
+echo "Applying get_mojo_to_build.patch"
 git apply $SCRIPT_DIR/get_mojo_to_build.patch
-git apply $SCRIPT_DIR/get_content_to_build.patch
-git apply $SCRIPT_DIR/get_chrome_to_build.patch
+echo "Applying add_mojo_base_include_dirs.patch"
+git apply $SCRIPT_DIR/add_mojo_base_include_dirs.patch
+echo "Applying fix_checkdeps.patch"
+git apply $SCRIPT_DIR/fix_checkdeps.patch
+echo "Adding build/config/mojo.gni"
 git add build/config/mojo.gni
+git commit -am "Changes from make_chromium_pull_changes.sh before updating Mojo pull"
+echo "Updating Mojo pull"
+cd $MOJO_SRC_DIR
+./mojo/tools/roll/rev_sdk.py $CHROMIUM_SRC_DIR
--- client_tools/chromium_pull_changes/search_and_replace_changes.py
+++ client_tools/chromium_pull_changes/search_and_replace_changes.py
@@ -10,4 +10,15 @@
 [r'(\.\./)(mojo/edk)', r'\1third_party/mojo/\2', ['*']],
 # Changing the include paths to be relative.
 [r'"mojo/services/public', r'"services/public', ['*cc', '*h', '*mojom']],
+# Changing the references to the gypfiles from the Mojo repo under //mojo.
+[r"'public/mojo_public.gyp", r"'../third_party/mojo/mojo/public/mojo_public.gyp", ["*gyp"]],
+[r"'edk/mojo_edk.gyp", r"'../third_party/mojo/mojo/edk/mojo_edk.gyp", ["*gyp"]],
+[r"'edk/mojo_edk_tests.gyp", r"'../third_party/mojo/mojo/edk/mojo_edk_tests.gyp", ["*gyp"]],
+[r"'services/public/mojo_services_public.gyp", r"'../third_party/mojo/services/public/mojo_services_public.gyp", ["*gyp"]],
+# Changing the references to the gypfiles from the Mojo repo via DEPTH.
+[r"<\(DEPTH\)/mojo/public", r"<(DEPTH)/third_party/mojo/mojo/public", ["*gyp", "*.gypi"]],
+[r"<\(DEPTH\)/mojo/edk", r"<(DEPTH)/third_party/mojo/mojo/edk", ["*gyp", "*.gypi"]],
+[r"<\(DEPTH\)/mojo/services/public", r"<(DEPTH)/third_party/mojo/services/public", ["*gyp", "*.gypi"]],
+# Changing references to download_shell_binary.py.
+[r"(mojo/public/tools/download_shell_binary.py)", r"third_party/mojo/\1", ["*"]],
 ]
