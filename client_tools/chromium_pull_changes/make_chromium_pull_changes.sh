a0d53fbec9ad5f909bfa94c92c4ae86dde929db8
Colin Blundell <blundell@chromium.org>
2015-01-05 16:44:39 +0100

    Updates to chromium move script to make things work with GN again (gyp not working at the moment)

--- client_tools/chromium_pull_changes/make_chromium_pull_changes.sh
+++ client_tools/chromium_pull_changes/make_chromium_pull_changes.sh
@@ -14,14 +14,25 @@ git mv mojo/edk third_party/mojo/edk
 #echo "Applying mojo_gni.patch"
 #git apply $SCRIPT_DIR/mojo_gni.patch
 $CHROMIUM_SRC_DIR/tools/git/mffr.py -fi $SCRIPT_DIR/search_and_replace_changes.py
-echo "Appling edk_js_test patch"
+# NOTE: The following two fixes actually need to be made Mojo-side.
+echo "Applying edk_js_test patch"
 git apply $SCRIPT_DIR/edk_js_test.patch
+echo "Applying mojo_public_buildfiles patch"
+git apply $SCRIPT_DIR/mojo_public_buildfiles.patch
+# NOTE: The following three fixes are in course of being submitted as separate
+# patches.
+echo "Applying chrome_test patch"
+git apply $SCRIPT_DIR/chrome_test.patch
+echo "Applying content_public_browser patch"
+git apply $SCRIPT_DIR/content_public_browser.patch
+echo "Applying html_viewer patch"
+git apply $SCRIPT_DIR/html_viewer.patch
 #echo "Applying add_landmine.patch"
 #git apply $SCRIPT_DIR/add_landmine.patch
 #echo "Applying gypfile_references_to_logging_files.patch"
 #git apply $SCRIPT_DIR/gypfile_references_to_logging_files.patch
-#echo "Applying get_mojo_to_build.patch"
-#git apply $SCRIPT_DIR/get_mojo_to_build.patch
+echo "Applying get_mojo_to_build.patch"
+git apply $SCRIPT_DIR/get_mojo_to_build.patch
 #echo "Applying add_mojo_base_include_dirs.patch"
 #git apply $SCRIPT_DIR/add_mojo_base_include_dirs.patch
 #echo "Applying fix_checkdeps.patch"
