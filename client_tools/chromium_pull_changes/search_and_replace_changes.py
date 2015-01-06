50b9fa6ed5a9df194fe35eb4451d394343adad78
Colin Blundell <blundell@chromium.org>
2015-01-06 09:57:51 +0100

    Changes toward having gyp work

--- client_tools/chromium_pull_changes/search_and_replace_changes.py
+++ client_tools/chromium_pull_changes/search_and_replace_changes.py
@@ -10,14 +10,15 @@
 [r'(\.\./)(mojo/edk)', r'\1third_party/\2', ['*']],
 # Changing the include paths to be relative.
 #[r'"mojo/services/public', r'"services/public', ['*cc', '*h', '*mojom']],
-# Changing the references to the gypfiles from the Mojo repo under //mojo.
-#[r"'public/mojo_public.gyp", r"'../third_party/mojo/mojo/public/mojo_public.gyp", ["*gyp"]],
-#[r"'edk/mojo_edk.gyp", r"'../third_party/mojo/mojo/edk/mojo_edk.gyp", ["*gyp"]],
-#[r"'edk/mojo_edk_tests.gyp", r"'../third_party/mojo/mojo/edk/mojo_edk_tests.gyp", ["*gyp"]],
+# Changing the references to the Mojo gypfiles from paths not under //mojo.
+[r"mojo/mojo_public.gyp", r"third_party/mojo/mojo_public.gyp", ["*gyp", "*.gypi"]],
+[r"mojo/mojo_edk.gyp", r"third_party/mojo/mojo_edk.gyp", ["*gyp", "*.gypi"]],
+[r"mojo/mojo_edk_tests.gyp", r"third_party/mojo/mojo_edk_tests.gyp", ["*gyp", "*.gypi"]],
+# Changing the references to the Mojo gypfiles under //mojo.
+[r"'mojo_public.gyp", r"'../third_party/mojo/mojo_public.gyp", ["mojo/*gyp"]],
+[r"'mojo_edk.gyp", r"'../third_party/mojo/mojo_edk.gyp", ["mojo/*gyp"]],
+[r"'mojo_edk_tests.gyp", r"'../third_party/mojo/mojo_edk_tests.gyp", ["mojo/*gyp"]],
 #[r"'services/public/mojo_services_public.gyp", r"'../third_party/mojo/services/public/mojo_services_public.gyp", ["*gyp"]],
-# Changing the references to the gypfiles from the Mojo repo via DEPTH.
-#[r"<\(DEPTH\)/mojo/public", r"<(DEPTH)/third_party/mojo/mojo/public", ["*gyp", "*.gypi"]],
-#[r"<\(DEPTH\)/mojo/edk", r"<(DEPTH)/third_party/mojo/mojo/edk", ["*gyp", "*.gypi"]],
 #[r"<\(DEPTH\)/mojo/services/public", r"<(DEPTH)/third_party/mojo/services/public", ["*gyp", "*.gypi"]],
 # Changing references to download_shell_binary.py.
 [r"(mojo/public/tools/download_shell_binary.py)", r"third_party/\1", ["*"]],
