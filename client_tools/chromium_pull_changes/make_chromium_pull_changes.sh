b0f45a683b8488123fdc97e23707f75fbd146b4f
Colin Blundell <blundell@chromium.org>
2015-01-06 08:26:13 +0100

    Additional fixes for chromium_pull_changes

--- client_tools/chromium_pull_changes/make_chromium_pull_changes.sh
+++ client_tools/chromium_pull_changes/make_chromium_pull_changes.sh
@@ -14,11 +14,15 @@ git mv mojo/edk third_party/mojo/edk
 #echo "Applying mojo_gni.patch"
 #git apply $SCRIPT_DIR/mojo_gni.patch
 $CHROMIUM_SRC_DIR/tools/git/mffr.py -fi $SCRIPT_DIR/search_and_replace_changes.py
-# NOTE: The following two fixes actually need to be made Mojo-side.
+# NOTE: The following two fixes have been made Mojo-side and can be eliminated
+# once Mojo is rolled into Chromium again.
 echo "Applying edk_js_test patch"
 git apply $SCRIPT_DIR/edk_js_test.patch
 echo "Applying mojo_public_buildfiles patch"
 git apply $SCRIPT_DIR/mojo_public_buildfiles.patch
+# NOTE: The following patch needs to be made in Mojo somehow.
+echo "Applying download_shell_binary patch"
+git apply $SCRIPT_DIR/download_shell_binary.patch
 # NOTE: The following three fixes are in course of being submitted as separate
 # patches.
 echo "Applying chrome_test patch"
@@ -27,8 +31,11 @@ echo "Applying content_public_browser patch"
 git apply $SCRIPT_DIR/content_public_browser.patch
 echo "Applying html_viewer patch"
 git apply $SCRIPT_DIR/html_viewer.patch
-#echo "Applying add_landmine.patch"
-#git apply $SCRIPT_DIR/add_landmine.patch
+
+echo "Applying mojom_bindings_generator_explicit patch"
+git apply $SCRIPT_DIR/mojom_bindings_generator_explicit.patch
+echo "Applying add_landmine.patch"
+git apply $SCRIPT_DIR/add_landmine.patch
 #echo "Applying gypfile_references_to_logging_files.patch"
 #git apply $SCRIPT_DIR/gypfile_references_to_logging_files.patch
 echo "Applying get_mojo_to_build.patch"
