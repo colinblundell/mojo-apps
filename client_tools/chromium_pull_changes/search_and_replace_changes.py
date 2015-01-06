c00cf8baee3d8068a2c75950d0dbae1de7421972
Colin Blundell <blundell@chromium.org>
2015-01-06 10:25:04 +0100

    Incremental changes toward runhooks working

--- client_tools/chromium_pull_changes/search_and_replace_changes.py
+++ client_tools/chromium_pull_changes/search_and_replace_changes.py
@@ -14,10 +14,15 @@
 [r"mojo/mojo_public.gyp", r"third_party/mojo/mojo_public.gyp", ["*gyp", "*.gypi"]],
 [r"mojo/mojo_edk.gyp", r"third_party/mojo/mojo_edk.gyp", ["*gyp", "*.gypi"]],
 [r"mojo/mojo_edk_tests.gyp", r"third_party/mojo/mojo_edk_tests.gyp", ["*gyp", "*.gypi"]],
+[r"(mojo/mojom_bindings_generator.gypi)", r"third_party/\1", ["*gyp", "*.gypi"]],
+[r"(mojo/mojom_bindings_generator_explicit.gypi)", r"third_party/\1", ["*gyp", "*.gypi"]],
+[r"(<\(DEPTH\))/(mojo/public)", r"\1/third_party/\2", ["*gyp", "*.gypi"]],
 # Changing the references to the Mojo gypfiles under //mojo.
 [r"'mojo_public.gyp", r"'../third_party/mojo/mojo_public.gyp", ["mojo/*gyp"]],
 [r"'mojo_edk.gyp", r"'../third_party/mojo/mojo_edk.gyp", ["mojo/*gyp"]],
 [r"'mojo_edk_tests.gyp", r"'../third_party/mojo/mojo_edk_tests.gyp", ["mojo/*gyp"]],
+[r"'mojo_variables.gypi", r"'../third_party/mojo/mojo_variables.gypi", ["mojo/*gyp"]],
+[r"'(mojom_bindings_generator.gypi)", r"'../third_party/mojo/\1", ["mojo/*gyp"]],
 #[r"'services/public/mojo_services_public.gyp", r"'../third_party/mojo/services/public/mojo_services_public.gyp", ["*gyp"]],
 #[r"<\(DEPTH\)/mojo/services/public", r"<(DEPTH)/third_party/mojo/services/public", ["*gyp", "*.gypi"]],
 # Changing references to download_shell_binary.py.
