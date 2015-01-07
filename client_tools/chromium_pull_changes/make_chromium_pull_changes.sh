74f1709a11cc5bafea2530c9ae91cf0558a160d5
Colin Blundell <blundell@chromium.org>
2015-01-07 14:22:15 +0100

    Update chromium pull changes

--- client_tools/chromium_pull_changes/make_chromium_pull_changes.sh
+++ client_tools/chromium_pull_changes/make_chromium_pull_changes.sh
@@ -6,18 +6,18 @@ CHROMIUM_SRC_DIR=$1
 cd $CHROMIUM_SRC_DIR
 echo "Moving code from Mojo public repo to live under //third_party/mojo"
 rm -rf third_party/mojo
-mkdir -p third_party/mojo
+mkdir -p third_party/mojo/mojo
 #mkdir -p third_party/mojo/services
-git mv mojo/public third_party/mojo/public
-git mv mojo/edk third_party/mojo/edk
-git mv mojo/mojo_public.gyp third_party/mojo
-git mv mojo/mojo_edk.gyp third_party/mojo
-git mv mojo/mojo_edk_system_impl.gypi third_party/mojo
-git mv mojo/mojo_edk_tests.gyp third_party/mojo
-git mv mojo/mojo_variables.gypi third_party/mojo
-git mv mojo/mojom_bindings_generator.gypi third_party/mojo
-git mv mojo/mojom_bindings_generator_explicit.gypi third_party/mojo
-git mv mojo/mojom_bindings_generator_variables.gypi third_party/mojo
+git mv mojo/public third_party/mojo/mojo/public
+git mv mojo/edk third_party/mojo/mojo/edk
+git mv mojo/mojo_public.gyp third_party/mojo/mojo
+git mv mojo/mojo_edk.gyp third_party/mojo/mojo
+git mv mojo/mojo_edk_system_impl.gypi third_party/mojo/mojo
+git mv mojo/mojo_edk_tests.gyp third_party/mojo/mojo
+git mv mojo/mojo_variables.gypi third_party/mojo/mojo
+git mv mojo/mojom_bindings_generator.gypi third_party/mojo/mojo
+git mv mojo/mojom_bindings_generator_explicit.gypi third_party/mojo/mojo
+git mv mojo/mojom_bindings_generator_variables.gypi third_party/mojo/mojo
 #git mv mojo/services/public third_party/mojo/services/public
 #echo "Applying mojo_gni.patch"
 #git apply $SCRIPT_DIR/mojo_gni.patch
@@ -33,15 +33,15 @@ echo "Applying download_shell_binary patch"
 git apply $SCRIPT_DIR/download_shell_binary.patch
 #echo "Applying chrome_test patch"
 #git apply $SCRIPT_DIR/chrome_test.patch
-echo "Applying content_public_browser patch"
-# NOTE: The following fixe is in course of being submitted as a separate
-# patch.
-git apply $SCRIPT_DIR/content_public_browser.patch
+# NOTE: The following 2 fixes are in course of being submitted as separate
+# patches.
+git apply $SCRIPT_DIR/nacl_ppapi_changes.patch
+git apply $SCRIPT_DIR/athena_deps.patch
 #echo "Applying html_viewer patch"
 #git apply $SCRIPT_DIR/html_viewer.patch
 
-echo "Applying mojom_bindings_generator_explicit patch"
-git apply $SCRIPT_DIR/mojom_bindings_generator_explicit.patch
+#echo "Applying mojom_bindings_generator_explicit patch"
+#git apply $SCRIPT_DIR/mojom_bindings_generator_explicit.patch
 echo "Applying add_landmine.patch"
 git apply $SCRIPT_DIR/add_landmine.patch
 #echo "Applying gypfile_references_to_logging_files.patch"
