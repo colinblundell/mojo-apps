5ae8e5e16eb697d10e6c5ed5d218312e878cf1d3
Colin Blundell <blundell@chromium.org>
2015-01-08 11:48:20 +0100

    Update to make_chromium_pull_changes.sh

--- client_tools/chromium_pull_changes/make_chromium_pull_changes.sh
+++ client_tools/chromium_pull_changes/make_chromium_pull_changes.sh
@@ -28,31 +28,39 @@ echo "Applying edk_js_test patch"
 git apply $SCRIPT_DIR/edk_js_test.patch
 echo "Applying mojo_public_buildfiles patch"
 git apply $SCRIPT_DIR/mojo_public_buildfiles.patch
-# NOTE: The following patch needs to be made in Mojo somehow.
+# NOTE: The following patch needs to land in Mojo and then get rolled into
+# Chromium.
 echo "Applying download_shell_binary patch"
 git apply $SCRIPT_DIR/download_shell_binary.patch
+
 #echo "Applying chrome_test patch"
 #git apply $SCRIPT_DIR/chrome_test.patch
 # NOTE: The following 2 fixes are in course of being submitted as separate
 # patches.
-git apply $SCRIPT_DIR/nacl_ppapi_changes.patch
-git apply $SCRIPT_DIR/athena_deps.patch
+#git apply $SCRIPT_DIR/nacl_ppapi_changes.patch
+#git apply $SCRIPT_DIR/athena_deps.patch
 #echo "Applying html_viewer patch"
 #git apply $SCRIPT_DIR/html_viewer.patch
 
 #echo "Applying mojom_bindings_generator_explicit patch"
 #git apply $SCRIPT_DIR/mojom_bindings_generator_explicit.patch
+#echo "Applying gypfile_references_to_logging_files.patch"
+#git apply $SCRIPT_DIR/gypfile_references_to_logging_files.patch
+
+# The following patches are all part of this CL and will land in the CL.
 echo "Applying add_landmine.patch"
 git apply $SCRIPT_DIR/add_landmine.patch
+echo "Applying add_license.patch"
 git apply $SCRIPT_DIR/add_license.patch
 git add third_party/mojo/LICENSE
 git add third_party/mojo/README.chromium
-#echo "Applying gypfile_references_to_logging_files.patch"
-#git apply $SCRIPT_DIR/gypfile_references_to_logging_files.patch
 echo "Applying get_mojo_to_build.patch"
 git apply $SCRIPT_DIR/get_mojo_to_build.patch
+echo "Applying mojo_base_logging_files.patch"
 git apply $SCRIPT_DIR/mojo_base_logging_files.patch
+echo "Applying gypfile_changes_for_mojo_public_to_build.patch"
 git apply $SCRIPT_DIR/gypfile_changes_for_mojo_public_to_build.patch
+echo "Applying get_mojo_edk_tests_to_build_in_gyp.patch"
 git apply $SCRIPT_DIR/get_mojo_edk_tests_to_build_in_gyp.patch
 echo "Applying add_mojo_base_include_dirs.patch"
 git apply $SCRIPT_DIR/add_mojo_base_include_dirs.patch
@@ -60,7 +68,8 @@ git apply $SCRIPT_DIR/add_mojo_base_include_dirs.patch
 #git apply $SCRIPT_DIR/fix_checkdeps.patch
 #echo "Adding build/config/mojo.gni"
 #git add build/config/mojo.gni
-git commit -am "Changes from make_chromium_pull_changes.sh"
+echo "Committing changes"
+git commit -am "Changes from make_chromium_pull_changes.sh" > /dev/null
 #echo "Updating Mojo pull"
 #cd $MOJO_SRC_DIR
 #./mojo/tools/roll/rev_sdk.py $CHROMIUM_SRC_DIR
