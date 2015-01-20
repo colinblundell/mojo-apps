2926e94cf72c8d4752714887480bd18ff15d4305
Colin Blundell <blundell@chromium.org>
2015-01-20 13:28:36 +0100

    Minor changes to move_services_in_chromium

--- client_tools/move_services_in_chromium/search_and_replace_changes.py
+++ client_tools/move_services_in_chromium/search_and_replace_changes.py
@@ -1,10 +1,7 @@
 [
-# Changing the dependencies on //mojo/public.
-[r'("//)(mojo/public[^"]*")', r'\1third_party/mojo/src/\2', ['*BUILD.gn']],
-[r'(\.\./)(mojo/public)', r'\1third_party/mojo/src/\2', ['*']],
 # Changing the dependencies on //mojo/services/public.
-#[r'("//)(mojo/services/public[^"]*")', r'\1third_party/\2', ['*BUILD.gn']],
-#[r'(\.\./)(mojo/services/public)', r'\1third_party/\2', ['*']],
+[r'("//)mojo/services/(public[^"]*")', r'\1third_party/mojo_services/src/\2', ['*BUILD.gn']],
+[r'(\.\./)(mojo/services/public)', r'\1third_party/\2', ['*']],
 # Changing the dependencies on //mojo/edk.
 [r'("//)(mojo/edk[^"]*")', r'\1third_party/mojo/src/\2', ['*BUILD.gn']],
 [r'(\.\./)(mojo/edk)', r'\1third_party/mojo/src/\2', ['*']],
