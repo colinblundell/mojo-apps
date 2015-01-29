e8da2cd0571eef9434adfaa7d9c47aa3bfe9fb8b
Colin Blundell <blundell@chromium.org>
2015-01-29 20:41:47 +0100

    Updates to moving services

--- client_tools/move_services_in_chromium/move_services_in_chromium.sh
+++ client_tools/move_services_in_chromium/move_services_in_chromium.sh
@@ -5,21 +5,16 @@ cd ~/chromium/src
 echo "Moving code from Mojo public services to live under //third_party/mojo_services"
 rm -rf third_party/mojo/services
 mkdir -p third_party/mojo_services/src
-# NOTE: mojo_sdk_root.gni has to be prenent in //mojo/services/mojo_sdk_root.gni
+# NOTE: mojo_sdk_root.gni has to be present in //mojo/services/mojo_sdk_root.gni
 # for the network service's public BUILD.gn file to work.
 cp mojo/services/mojo_sdk_root.gni third_party/mojo_services/src
 git add third_party/mojo_services/src/mojo_sdk_root.gni
-git mv mojo/mojo_services_public.gyp third_party/mojo_services
 git mv mojo/services/public third_party/mojo_services/src/public
 for d in accessibility clipboard content_handler geometry gpu input_events native_viewport navigation surfaces view_manager window_manager; do
   git mv mojo/services/$d third_party/mojo_services/src/$d
 done
-# Note: The network service has an impl that belongs in Chromium and thus should
-# stay where it is.
-#mkdir third_party/mojo_services/src/network
-#git mv mojo/services/network/public third_party/mojo_services/src/network/public
 
-git commit -am "move_services_in_chromium.sh: Move Mojo services code"
+git commit -am "move_services_in_chromium.sh: Move Mojo services code" > /dev/null
 commit_after_code_move=`git rev-parse HEAD`
 
 ./tools/git/mffr.py -fi $SCRIPT_DIR/search_and_replace_changes.py
@@ -36,24 +31,9 @@ git add third_party/mojo_services/README.chromium
 echo "Applying make_owners_file_changes"
 git apply $SCRIPT_DIR/make_owners_file_changes.patch
 git add third_party/mojo_services/OWNERS
-echo "Applying mojo_services_public_network"
-git apply $SCRIPT_DIR/mojo_services_public_network.patch
-
-# This patch should be replaced by having this script move
-# network_service_root.gni to //third_party/mojo_services/src once
-# the gni file exists on origin/master in Chromium.
-echo "Applying network_service_root"
-git apply $SCRIPT_DIR/network_service_root.patch
-
-# These patches are separate Chromium CLs that can be removed once
-# they land in Chromium.
-echo "Applying network_service_buildfile"
-git apply $SCRIPT_DIR/network_service_buildfile.patch
-
-# These patches are Mojo-side CLs that need to land and roll into
-# Chromium and then can be removed here.
-echo "Applying mojo_service_buildfiles"
-git apply $SCRIPT_DIR/mojo_service_buildfiles.patch
+echo "Applying mojo_services_public_config"
+git apply $SCRIPT_DIR/mojo_services_public_config.patch
+git add mojo/services/public/build/config/BUILD.gn
 
 git commit -am "move_services_in_chromium.sh: Functional changes" > /dev/null
 
@@ -65,10 +45,16 @@ echo "Reordering references in buildfiles"
 for f in `git diff --name-only HEAD~2`; do
   $SCRIPT_DIR/reorder_references_in_buildfiles.py $f
 done
-git commit -am "move_services_in_chromium.sh: Reorder references in buildfiles"
-#echo "Redoing Mojo pull as a check against inserting Mojo-side changes"
-#~/mojo/src/mojo/tools/roll/rev_sdk.py $CHROMIUM_SRC_DIR
+git commit -am "move_services_in_chromium.sh: Reorder references in buildfiles" > /dev/null
 
 echo "Restoring //third_party/mojo_services/src to a pristine state"
 git checkout $commit_after_code_move -- third_party/mojo_services/src
-git commit -am "move_services_in_chromium.sh: Restore Mojo services code to pristine state"
+git commit -am "move_services_in_chromium.sh: Restore Mojo services code to pristine state" > /dev/null
+
+echo "Adding Mojo-side patches that need to be rolled into Chromium before this CL can land"
+
+# These patches are Mojo-side CLs that need to land and roll into
+# Chromium and be removed here before this CL can land.
+echo "Applying mojo_service_buildfiles"
+git apply $SCRIPT_DIR/mojo_service_buildfiles.patch
+git commit -am "Mojo-side changes that need to be rolled in to Chromium" > /dev/null
--- client_tools/move_services_in_chromium/search_and_replace_changes.py
+++ client_tools/move_services_in_chromium/search_and_replace_changes.py
@@ -3,13 +3,6 @@
 [r'(//)mojo/services/([^/]*/public[^"]*")', r'\1third_party/mojo_services/src/\2', ['*BUILD.gn']],
 # Changing the include paths.
 [r'"mojo/services/([^/]*/public)', r'"third_party/mojo_services/src/\1', ['*cc', '*h', '*mojom']],
-# Changing the references to the Mojo services gypfile under //mojo.
-[r"'mojo_services_public.gyp", r"'../third_party/mojo_services/mojo_services_public.gyp", ["mojo/*gyp"]],
-# Fixing up mojo_services_public.gyp.
-[r'(//)mojo/services/([^/]*/public[^ ]*)', r'\1third_party/mojo_services/src/\2', ["*mojo_services_public.gyp"]],
 # Change back references to the network service.
 [r'third_party/mojo_services/src/network', r'mojo/services/network', ['*']],
-[r"\-Iservices", r"-Isrc", ["*mojo_services_public.gyp"]],
-[r"'services", r"'src", ["*mojo_services_public.gyp"]],
-[r"\.\./third_party", r"..", ["*mojo_services_public.gyp"]],
 ]
